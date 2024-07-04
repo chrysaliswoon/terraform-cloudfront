e4774a426165fc2dd22e5063d611ff3020602bb0
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2024-07-04 09:23:46 -0400

    changed index document

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "bucket" {
   acl    = "public-read"
 
   website {
-    index_document = "index.html"
+    index_document = "homr.html"
     error_document = "error.html"
   }
 }
